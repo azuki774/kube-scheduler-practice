56336b93e6814193f1ad659ccbec8c0d99b61196
azuki774 <azuki774s@gmail.com>
2025-07-16 18:59:44 +0900

    initial commit

---
+++ cmd/local.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"context"
+	"flag"
+	"log"
+	"log/slog"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+// localCmd represents the local command
+var localCmd = &cobra.Command{
+	Use:   "local",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		} else {
+			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		}
+		flag.Parse()
+
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			log.Fatalf("Error building kubeconfig: %s", err.Error())
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			log.Fatalf("Error creating clientset: %s", err.Error())
+		}
+
+		nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			log.Fatalf("Error getting nodes: %s", err.Error())
+		}
+
+		for _, node := range nodes.Items {
+			slog.Info("node", "name", node.Name)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(localCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// localCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// localCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
